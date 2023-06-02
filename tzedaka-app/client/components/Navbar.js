dc7a3acb2e5daca4ea1b5679b456c429dc2bbc29
jmestanza-ffg <jmestanza@itba.edu.ar>
2023-06-02 12:43:53 -0300

    added navbar with styles

--- tzedaka-app/client/components/Navbar.js
+++ tzedaka-app/client/components/Navbar.js
@@ -3,19 +3,42 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    // <nav className={styles.mainnav}>
-    <nav>
-      <ul>
-        <Link href={"/"}>
-          <li>Inicio</li>
-        </Link>
-        <Link href="/programs">
-          <li>Programas</li>
-        </Link>
-        <Link href="/form">
-          <li>Contacto</li>
-        </Link>
-      </ul>
+    <nav className="flex items-center justify-between flex-wrap bg-white-500 p-6">
+      <div>
+        <img
+          src={
+            "https://tzedaka.org.ar/wp-content/uploads/2017/12/logo-tzedaka.png"
+          }
+          style={{ height: "40px" }}
+          alt={""}
+          loading={"lazy"}
+        />
+      </div>
+      <div className="flex items-center justify-between flex-wrap space-x-5 list-none bg-white">
+        <div className="pr-2 text-l font-semibold text-black">
+          <Link href={"/"}>
+            <li>Inicio</li>
+          </Link>
+        </div>
+        <div className="pr-2 text-l font-semibold text-black">
+          <Link href={"/programs"}>
+            <li>Programas</li>
+          </Link>
+        </div>
+        <div className="pr-2 text-l font-semibold text-black">
+          <Link href={"/form"}>
+            <li>Contacto</li>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <a
+          href="#"
+          className="inline-block text-l font-semibold px-4 py-2 leading-none border rounded text-black border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        >
+          Donar
+        </a>
+      </div>
     </nav>
   );
 };
