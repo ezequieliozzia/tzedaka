e40efa1561910c846ee518475d966b3d81bc5a3a
camimargni <camiimargni@mail.com>
2023-06-16 16:40:20 -0300

    datos del usuario logeado

--- tzedaka-app/client/pages/index.js
+++ tzedaka-app/client/pages/index.js
@@ -1,8 +1,11 @@
 import Layout from "@/components/Layout";
 import React from "react";
+import { useUser } from '@clerk/nextjs';
 
 const index = () => {
-  return <div>index</div>;
+  const { user } = useUser();
+  return <div className="h-85v">index {user?.lastName}</div>;
 };
 
 export default index;
+
