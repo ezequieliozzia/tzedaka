76a64f2c867c8f506452064a47e2367642da3c4f
jmestanza-ffg <jmestanza@itba.edu.ar>
2023-06-30 15:24:16 -0300

    testing db+front connection

--- tzedaka-app/client/pages/profile.js
+++ tzedaka-app/client/pages/profile.js
@@ -1,8 +1,27 @@
 import Godchild from "@/components/Godchild";
 import Godfather from "@/components/Godfather";
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
+  // console.log("Arranca el fetch");
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("http://localhost:5000/getUser", {
+        method: "GET",
+        cache: "no-store",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data ", data);
+          setUsers(data.records);
+        });
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <Godfather></Godfather>
