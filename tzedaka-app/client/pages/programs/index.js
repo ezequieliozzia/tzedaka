a05f88cda02b1b694d0dc625e70d78fc2f55d1ee
Juan Serra <juan.cb.serra@tsgforce.com>
2023-07-20 17:10:41 -0300

    implementacion basica de card para programas

--- tzedaka-app/client/pages/programs/index.js
+++ tzedaka-app/client/pages/programs/index.js
@@ -1,7 +1,57 @@
 import React from "react";
+import image from "../../images/kid.png";
 
-const index = () => {
-  return <div>Programs</div>;
+const programa_1 = {
+  title: "Title programa 1",
+  text: `Aliqua ipsum exercitation enim eu Lorem Lorem sit veniam est cupidatat amet. Pariatur consectetur tempor do sint velit ullamco proident non magna Lorem ex enim. Eu ex deserunt commodo proident esse eu dolor enim aliquip dolor reprehenderit fugiat do
+Occaecat officia ex cillum mollit tempor culpa minim laboris. Veniam velit exercitation deserunt consectetur tempor nisi sunt laborum. Consectetur in veniam laboris officia qui. Tempor cillum dolore nostrud cupidatat cupidatat.`,
 };
 
-export default index;
+const Programs = () => {
+  return (
+    <>
+      <Card programa={programa_1} />
+      <Card programa={programa_1} />
+      <Card programa={programa_1} />
+    </>
+  );
+};
+
+const Card = ({
+  programa
+}) => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "90%",
+        backgroundColor: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        textAlign: "center",
+        padding: "20px",
+        margin: "5%",
+        gap: "20px",
+      }}
+    >
+      <span
+        style={{
+          color: "#000",
+          fontFamily: "Abhaya Libre ExtraBold",
+          fontSize: "30px",
+          fontStyle: "normal",
+          fontWeight: "800",
+          lineHeight: "22px",
+        }}
+      >
+        {programa.title}
+      </span>
+      <img style={{ width: "50%" }} src={image.src} alt="IMAGEN" />
+      <p>{programa.text}</p>
+    </div>
+  );
+};
+
+export default Programs;
