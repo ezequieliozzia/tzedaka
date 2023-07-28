dc2c62ba3d3821334c0c5d664e80512e74191b70
juanchi-ffg <juan.m.suarez@tsgforce.com>
2023-07-28 16:52:27 -0300

    Primeros datos en el detalle

--- tzedaka-app/client/pages/programs/details/[programId].js
+++ tzedaka-app/client/pages/programs/details/[programId].js
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/router';
-import programs from "../../../public/mocks/programs.json"
+import { useRouter } from "next/router";
+import programs from "../../../public/mocks/programs.json";
 
 function Post() {
   const router = useRouter();
@@ -7,8 +7,66 @@ function Post() {
 
   return (
     <div>
-      <h1>{programs[programId].programName}</h1>
-      <h2>{programs[programId].programDescription}</h2>
+      <div
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.60)",
+          padding: "1% 1%",
+          display: "flex",
+          justifyContent: "center",
+          fillOpacity: "0.6",
+        }}
+      >
+        <span
+          style={{
+            color: "#000",
+            fontFamily: "Abhaya Libre ExtraBold",
+            fontSize: "40px",
+            fontStyle: "normal",
+            fontWeight: "800",
+            lineHeight: "22px" /* 55% */,
+          }}
+        >
+          {programs[programId].programName}
+        </span>
+      </div>
+
+      <div
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.60)",
+          fillOpacity: "0.6",
+          padding: "1% 1%",
+          display: "inline-block",
+          marginTop: "2%",
+        }}
+      >
+        <span
+          style={{
+            color: "#000",
+            fontFamily: "Abhaya Libre ExtraBold",
+            fontSize: "30px",
+            fontStyle: "normal",
+            fontWeight: "800",
+            lineHeight: "22px",
+          }}
+        >
+          Acerca de {programs[programId].programName}
+        </span>
+      </div>
+      <span
+        style={{
+          color: "#000",
+          fontFamily: "Abhaya Libre ExtraBold",
+          fontSize: "25px",
+          fontStyle: "normal",
+          fontWeight: "800",
+          lineHeight: "25px",
+          display: "flex",
+          justifyContent: "center",
+          margin: "5% 2%",
+        }}
+      >
+        {programs[programId].programDescription}
+      </span>
     </div>
   );
 }
