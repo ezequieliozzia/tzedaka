845c954ca32821f66320aac1b27af88cdec46912
jmestanza-ffg <jmestanza@itba.edu.ar>
2023-08-18 17:49:42 -0300

    fixed public routes and refactored Navbar

--- tzedaka-app/client/components/Navbar.js
+++ tzedaka-app/client/components/Navbar.js
@@ -1,11 +1,10 @@
-import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button } from "antd";
 import { ArrowRightOutlined } from "@ant-design/icons";
 import { latoBold700 } from "../utils/fonts";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import NavbarButton from "./NavbarButton";
 
 const Navbar = () => {
   const { push } = useRouter();
@@ -26,60 +25,44 @@ const Navbar = () => {
         />
       </div>
       <div className="flex items-center justify-between flex-wrap space-x-8 list-none bg-transparent">
-        <div className={`pr-2 text-l font-semibold text-black ${latoBold700.className}`}>
-          <Link href={"/"}>
-            <li>Inicio</li>
-          </Link>
-        </div>
-        <div className={`pr-2 text-l font-semibold text-black ${latoBold700.className}`}>
-          <Link href={"/programs"}>
-            <li>Programas</li>
-          </Link>
-        </div>
+        <NavbarButton text={"Inicio"} href={"/"} />
+        <NavbarButton text={"Programas"} href={"/programs"} />
         <SignedIn>
-          <div className={`pr-2 text-l font-semibold text-black ${latoBold700.className}`}>
-            <Link href={"/form"}>
-                <li>Perfil</li>
-            </Link>
-          </div>
-        </SignedIn>  
-        <div className={`pr-2 text-l font-semibold text-black ${latoBold700.className}`}>
-          <Link href={"/form"}>
-            <li>Contacto</li>
-          </Link>
-        </div>
-
-        <div className={`pr-2 text-l font-semibold text-black ${latoBold700.className}`}>
-          <Link href={"/form"}>
-            <li>Sobre Nosotros</li>
-          </Link>
-        </div>
+          <NavbarButton text={"Perfil"} href={"/profile"} />
+        </SignedIn>
+        <NavbarButton text={"Contacto"} href={"/form"} />
+        <NavbarButton
+          text={"Sobre Nosotros"}
+          href={"https://tzedaka.org.ar/la-fundacion/"}
+          target={"_blank"}
+        />
       </div>
       <div className="flex items-center justify-center flex-nowrap space-x-5 list-none bg-transparent">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+          <button class="relative h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
+            <span class="relative z-10">Donar</span>
+          </button>
+        </SignedIn>
 
-      <SignedIn>
-        <UserButton afterSignOutUrl="/" />
-        <button class="relative h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
-          <span class="relative z-10">Donar</span>
-        </button>
-      </SignedIn>
+        <SignedOut>
+          <Button
+            type="default"
+            shape="round"
+            size="large"
+            icon={<ArrowRightOutlined />}
+            onClick={handleLogIn}
+          >
+            Ingresar
+          </Button>
 
-      <SignedOut>
-        <Button
-              type="default"
-              shape="round"
-              size="large"
-              icon={<ArrowRightOutlined />}
-              onClick={handleLogIn}
-        >
-          Ingresar
-        </Button>
-
-        <button onClick={handleLogIn} class="relative h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
-          <span class="relative z-10">Donar</span>
-        </button>
-
-      </SignedOut>
+          <button
+            onClick={handleLogIn}
+            class="relative h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80"
+          >
+            <span class="relative z-10">Donar</span>
+          </button>
+        </SignedOut>
       </div>
     </nav>
   );
--- tzedaka-app/client/pages/programs/index.js
+++ tzedaka-app/client/pages/programs/index.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 const index = () => {
-  return <div>Programs</div>;
+  return (
+    <>
+      <SignedIn>
+        <div>Programs</div>
+      </SignedIn>
+      <SignedOut>
+        <div>Programs</div>
+      </SignedOut>
+    </>
+  );
 };
 
 export default index;
