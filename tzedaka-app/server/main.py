628ad73a9a15e33739bc23e3f2c0f569ac6e8ac9
fgs222 <134948041+FGSFFG@users.noreply.github.com>
2023-09-22 16:56:39 -0300

    Endpoint Ahijados no apadrianados

--- tzedaka-app/server/main.py
+++ tzedaka-app/server/main.py
@@ -16,8 +16,8 @@ sf = Salesforce( username        = creds['login']['username'],
                  consumer_key    = creds['login']['consumer_key'],
                  consumer_secret = creds['login']['consumer_secret'] )
 
-customEntities = sf.query("SELECT Id, Name  FROM Beneficiario__c LIMIT 10")
-accountEntities = sf.query("SELECT Id, Name  FROM Account LIMIT 10")
+queryAhijados = sf.query("SELECT Name, Edad__c, Programa__c, PAG_Descripci_n__c, C_digo_del_beneficiario__c FROM Beneficiario__c")
+queryPadrinazgos = sf.query("SELECT Ahijado__c FROM Padrinazgo__c")
 
 activeUser = 0
 
@@ -81,6 +81,41 @@ def contactInfo():
                 data = {}
 
         return jsonify(data)
+
+
+# Es necesario que se encuentren logueados para poder ver esto? Imagino que no
+@app.route("/beneficiaries", methods=['POST', 'GET'])
+@cross_origin()
+def beneficiaries():
+    if request.method == 'GET':
+        # query = sf.query("SELECT Name, Edad__c, Programa__c, PAG_Descripci_n__c FROM Beneficiario__c WHERE C_digo_del_beneficiario__c NOT IN (SELECT Ahijado__c FROM Padrinazgo__c)")
+        queryAhijados = sf.query("SELECT Name, Edad__c, Programa__c, PAG_Descripci_n__c FROM Beneficiario__c")
+        queryPadrinazgos = sf.query("SELECT Ahijado__r.Name FROM Padrinazgo__c")
+
+        ahijados = []
+        padrinazgos = []
+        noApadrinados = []
+
+        for entry in range(0, queryAhijados['totalSize']):
+            
+            data = { "Nombre": queryAhijados['records'][entry]['Name'],
+                     "Edad": queryAhijados['records'][entry]['Edad__c'],
+                     "Programa": queryAhijados['records'][entry]['Programa__c'],
+                     "Descripcion": queryAhijados['records'][entry]['PAG_Descripci_n__c']
+            }
+
+            ahijados.append(data)
+            
+        for entry in range(0, queryPadrinazgos['totalSize']):
+            # Si el padrinazgo no tiene ahijado rompe todo
+            if ( queryPadrinazgos['records'][entry]['Ahijado__r'] != None ):
+                padrinazgos.append( queryPadrinazgos['records'][entry]['Ahijado__r']['Name'] )
+
+        for x in range(0, len(ahijados)):
+            ahijados[x]["Nombre"] not in padrinazgos and noApadrinados.append(ahijados[x])
+
+        return jsonify({"ahijados": noApadrinados})
+
 
 # Queries de prueba
 
