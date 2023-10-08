ab4d5506428805f37d05e895c417e3cf55f6cb42
jmestanza <jmestanza@itba.edu.ar>
2023-10-08 18:13:01 -0300

    restored border

--- tzedaka-app/client/pages/index.js
+++ tzedaka-app/client/pages/index.js
@@ -5,11 +5,11 @@ import { universe45Light, universe65Bold } from "../utils/fonts";
 const Index = () => {
   return (
     <>
-      <div className="relative h-84vh mx-6 rounded-md">
+      <div className="relative h-84vh mx-6 rounded-lg overflow-hidden">
         <Image src={bg.src} fill style={{ objectFit: "cover" }} />
         <div className="absolute inset-0 bg-gray-900 bg-opacity-50"></div>
         <div className="absolute inset-0 flex md:items-start md:my-20 md:justify-center md:justify-end">
-          <div className="flex flex-col items-center md:items-center md:justify-center md:w-2/6 md:h-1/3 bg-transparent md:mr-20 rounded-md">
+          <div className="flex flex-col items-center md:items-center md:justify-center md:w-2/6 md:h-1/3 bg-transparent md:mr-20">
             <p
               className={`mt-4 md:text-4xl font-bold text-white ${universe65Bold.className}`}
             >
