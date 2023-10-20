30ae051feac72e07c5c22dda5841e9172be419df
pipoFFG <pedro.aguirre@tsgforce.com>
2023-10-20 18:52:06 -0300

    Changing styles in hover for kids under programs

--- tzedaka-app/client/components/programs/ProgramsCard.js
+++ tzedaka-app/client/components/programs/ProgramsCard.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const ProgramsCard = ({ programs, kid, programId }) => {
   return (
-    <div key={kid.id} className="overflow-hidden bg-violet-50 cursor-pointer rounded-xl relative group h-full w-full flex items-center justify-center">
+    <div key={kid.id} className="relative overflow-hidden bg-violet-50 cursor-pointer rounded-xl group h-full w-full">
       <div className="flex items-center flex-col mt-5">
-        <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/90 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
+        <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out cursor-pointer absolute inset-0 bg-black/90 text-white flex items-center justify-center">
           <div>
-            <div className="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
+            <div className="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-500 ease-in-out">
               <span className="text-s w-3/4 mb-5">{kid.description}</span>
             </div>
           </div>
