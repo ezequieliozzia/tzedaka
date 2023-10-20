b5c67f77c6569f642c0740ee69c894f02923ba74
jmestanza <jmestanza@itba.edu.ar>
2023-10-20 12:10:03 -0300

    deleting CORS

--- tzedaka-app/server/api/index.py
+++ tzedaka-app/server/api/index.py
@@ -7,7 +7,7 @@ activeUser = 0
 app = Flask(__name__)
 
 # CORS(app, resources={r"/api/*": {"origins":"https://tzedaka-neon.vercel.app" }})
-app.config['CORS_HEADERS'] = 'Content-Type'
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.register_blueprint(blueprint, url_prefix='/')
 
