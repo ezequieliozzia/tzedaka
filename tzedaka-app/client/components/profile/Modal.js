7931e4304682cae9c278cc8748924592884f8652
jmestanza <jmestanza@itba.edu.ar>
2023-10-27 18:27:56 -0300

    added modal

--- tzedaka-app/client/components/profile/Modal.js
+++ tzedaka-app/client/components/profile/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel from "../programs/Carousel";
 
 const Modal = ({ rowSelected, setRowSelected }) => {
   return (
@@ -25,13 +26,8 @@ const Modal = ({ rowSelected, setRowSelected }) => {
                 </div>
               </div>
             </div>
+            <Carousel />
             <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              {/* <button
-                type="button"
-                class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-              >
-                Deactivate
-              </button> */}
               <button
                 type="button"
                 class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
