0072f4939ec99c238caa5c5e6526fc89174905ca
pipoFFG <pedro.aguirre@tsgforce.com>
2023-11-12 23:43:45 -0300

    Created StyledNavbarButton component for navbar

--- tzedaka-app/client/components/Navbar.js
+++ tzedaka-app/client/components/Navbar.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Link from "next/link"
-import { useRouter } from "next/navigation";
 import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import NavbarButton from "./NavbarButton";
 import tzedakaLogo from "../images/logo-tzedaka.png";
+import StyledNavbarButton from "./StyledNavbarButton";
 
 const Navbar = () => {
   return (
@@ -28,20 +27,11 @@ const Navbar = () => {
       <div className="flex items-center justify-center flex-nowrap space-x-5 list-none bg-transparent">
         <SignedIn>
           <UserButton afterSignOutUrl="/" />
-          <Link href="/form" className="relative inline-flex items-center justify-center h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
-            <span className="relative z-10">Donar</span>
-          </Link>
+          <StyledNavbarButton href="/form" label="Donar" />
         </SignedIn>
-
         <SignedOut>
-          <Link href="/signInPage" className="relative inline-flex items-center justify-center h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
-            <span className="relative z-10">Ingresar</span>
-          </Link>
-
-          <Link href="/signInPage" className="relative inline-flex items-center justify-center h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
-            <span className="relative z-10">Donar</span>
-          </Link>
-
+          <StyledNavbarButton href="/signInPage" label="Ingresar" />
+          <StyledNavbarButton href="/signInPage" label="Donar" />
         </SignedOut>
       </div>
     </nav>
---
+++ tzedaka-app/client/components/StyledNavbarButton.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+const StyledNavbarButton = ({ href, label }) => {
+    return (
+      <Link
+        href={href}
+        className="relative inline-flex items-center justify-center h-12 w-24 rounded-full overflow-hidden border border-indigo-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80"
+      >
+        <span className="relative z-10">{label}</span>
+      </Link>
+    );
+  };
+
+export default StyledNavbarButton;
