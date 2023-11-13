1ad94ed2ee49e51b1b120f0cf05ee210eb21fb98
pipoFFG <pedro.aguirre@tsgforce.com>
2023-11-13 17:34:48 -0300

    Adding sharing options with multiple social media

--- tzedaka-app/client/pages/index.js
+++ tzedaka-app/client/pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import bg from "../images/children-playing.jpg";
 import StyledLinkButton from "@/components/StyledLinkButton";
 import HomeMainText from "@/components/HomeMainText";
+import ShareButtons from "@/components/ShareButtons";
 
 const Index = () => {
   return (
@@ -12,8 +13,9 @@ const Index = () => {
         <div className="flex mt-8 space-x-4">
           <StyledLinkButton href="/profile" label="Soy Padrino"     bgColor="bg-purple-800" textColor="text-white" hoverBgColor="bg-purple-200" hoverTextColor="text-white-800"/>
           <StyledLinkButton href="/form" label="Quiero Apadrinar"   bgColor="bg-purple-800" textColor="text-white" hoverBgColor="bg-purple-200" hoverTextColor="text-white-800"/>
-          <StyledLinkButton href="/" label="Invitar a ser Padrino"  bgColor="bg-purple-800" textColor="text-white" hoverBgColor="bg-purple-200" hoverTextColor="text-white-800"/>
+          {/* <StyledLinkButton href="/" label="Invitar a ser Padrino"  bgColor="bg-purple-800" textColor="text-white" hoverBgColor="bg-purple-200" hoverTextColor="text-white-800"/> */}
         </div>
+        <ShareButtons/>
       </div>
     </div>
   );
