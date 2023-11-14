ebddb8825e27f98d3084a5efe8d956dddfdc70e5
pipoFFG <pedro.aguirre@tsgforce.com>
2023-11-14 16:04:42 -0300

    Minor changes in Program Card

--- tzedaka-app/client/pages/programs/index.js
+++ tzedaka-app/client/pages/programs/index.js
@@ -21,16 +21,15 @@ const Programs = () => {
 
 const Card = ({ programa }) => {
   return (
-    <>
       <Link href={`/programs/details/${programa.programId}`}>
         <div className="flex items-center justify-center">
           <div className="overflow-hidden bg-gray-100 cursor-pointer rounded-xl relative group h-10/12 w-10/12 flex items-center justify-center">
-            <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/90 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
-              <div>
-                <div className="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
-                  <div className="font-bold"> {programa.programName} </div>
-                  <div className="text-sm"> {programa.programDescription} </div>
-                </div>
+            {/* <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer absolute from-black/90 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end"> */}
+            {/* <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out cursor-pointer absolute bottom-0 bg-black/90 text-white flex items-center justify-center h-1/w-full"> */}
+            <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out cursor-pointer absolute inset-0 bg-black/90 text-white flex items-center justify-center">
+              <div className="transform-gpu p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">
+                <div className="font-bold"> {programa.programName} </div>
+                <div className="text-sm"> {programa.programDescription} </div>
               </div>
             </div>
             <img
@@ -41,7 +40,6 @@ const Card = ({ programa }) => {
           </div>
         </div>
       </Link>
-    </>
   );
 };
 
